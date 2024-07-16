a96ba311bed6161d3862b87032f980772dad6692
mazzac15 <114017171+mazzac15@users.noreply.github.com>
2024-07-15 22:52:17 -0400

    updates to html home for deploy

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         search: resolve(__dirname, "src/search/index.html"),
-        recipe_detail: resolve(__dirname, "src/recipe-detail/index.html"),
+        recipe: resolve(__dirname, "src/recipe-detail/index.html"),
         collections: resolve(__dirname, "src/collections/index.html"),
         profile: resolve(__dirname, "src/profile/index.html"),
       },
